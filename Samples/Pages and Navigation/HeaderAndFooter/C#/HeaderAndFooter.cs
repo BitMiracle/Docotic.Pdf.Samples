546e07adede06a5e8d1f76e362252d82a3067568
Vitaly Shibaev <shibvit@gmail.com>
2019-06-05 21:02:58 +0600

    Add "Header and Footer" sample

---
+++ Samples/Pages and Navigation/HeaderAndFooter/C#/HeaderAndFooter.cs
@@ -0,0 +1,103 @@
+using System.Diagnostics;
+
+namespace BitMiracle.Docotic.Pdf.Samples
+{
+    public static class HeaderAndFooter
+    {
+        public static void Main()
+        {
+            // NOTE:
+            // When used in trial mode, the library imposes some restrictions.
+            // Please visit http://bitmiracle.com/pdf-library/trial-restrictions.aspx
+            // for more information.
+            
+            const string PathToFile = "HeaderAndFooter.pdf";
+
+            using (var pdf = new PdfDocument(@"Sample Data/BRAILLE CODES WITH TRANSLATION.pdf"))
+            {
+                PdfFont font = pdf.AddFont(PdfBuiltInFont.Helvetica);
+
+                for (int i = 0; i < pdf.PageCount; ++i)
+                {
+                    PdfPage page = pdf.Pages[i];
+                    drawHeader(page, font);
+                    drawFooter(i, page, font);
+                }
+
+                pdf.Save(PathToFile);
+            }
+
+            Process.Start(PathToFile);
+        }
+
+        private static void drawHeader(PdfPage page, PdfFont font)
+        {
+            const string HeaderText = "Header";
+
+            var canvas = page.Canvas;
+            canvas.Font = font;
+            canvas.FontSize = 30;
+
+            double textWidth = canvas.GetTextWidth(HeaderText);
+            double rotatedPageWidth = getRotatedPageSize(page).Width;
+            var headerPosition = new PdfPoint((rotatedPageWidth - textWidth) / 2, 10);
+
+            showTextAtRotatedPage(HeaderText, headerPosition, page);
+        }
+
+        private static void drawFooter(int pageIndex, PdfPage page, PdfFont font)
+        {
+            var canvas = page.Canvas;
+            canvas.Font = font;
+            canvas.FontSize = 14;
+
+            PdfSize rotatedPageSize = getRotatedPageSize(page);
+            var paddingFromCorner = new PdfSize(30, 30);
+            var positionRightBottom = new PdfPoint(
+                rotatedPageSize.Width - paddingFromCorner.Width,
+                rotatedPageSize.Height - paddingFromCorner.Height
+            );
+            
+            showTextAtRotatedPage((pageIndex + 1).ToString(), positionRightBottom, page);
+        }
+
+        private static PdfSize getRotatedPageSize(PdfPage page)
+        {
+            if (page.Rotation == PdfRotation.Rotate90 || page.Rotation == PdfRotation.Rotate270)
+                return new PdfSize(page.Height, page.Width);
+
+            return new PdfSize(page.Width, page.Height);
+        }
+
+        private static void showTextAtRotatedPage(string text, PdfPoint position, PdfPage page)
+        {
+            PdfCanvas canvas = page.Canvas;
+            if (page.Rotation == PdfRotation.None)
+            {
+                canvas.DrawString(position.X, position.Y, text);
+                return;
+            }
+
+            canvas.SaveState();
+
+            switch (page.Rotation)
+            {
+                case PdfRotation.Rotate90:
+                    canvas.TranslateTransform(position.Y, page.Height - position.X);
+                    break;
+
+                case PdfRotation.Rotate180:
+                    canvas.TranslateTransform(page.Width - position.X, page.Height - position.Y);
+                    break;
+
+                case PdfRotation.Rotate270:
+                    canvas.TranslateTransform(page.Width - position.Y, position.X);
+                    break;
+            }
+            canvas.RotateTransform((int)page.Rotation * 90);
+            canvas.DrawString(0, 0, text);
+
+            canvas.RestoreState();
+        }
+    }
+}
---
+++ Samples/Pages and Navigation/HeaderAndFooter/VB.NET/HeaderAndFooter.vb
@@ -0,0 +1,88 @@
+Imports System.Diagnostics
+
+Imports BitMiracle.Docotic.Pdf
+
+Namespace BitMiracle.Docotic.Pdf.Samples
+    Public NotInheritable Class HeaderAndFooter
+        Public Shared Sub Main()
+            ' NOTE:
+            ' When used in trial mode, the library imposes some restrictions.
+            ' Please visit http://bitmiracle.com/pdf-library/trial-restrictions.aspx
+            ' for more information.
+
+            Const PathToFile As String = "HeaderAndFooter.pdf"
+
+            Using pdf = New PdfDocument("Sample Data/BRAILLE CODES WITH TRANSLATION.pdf")
+                Dim font As PdfFont = pdf.AddFont(PdfBuiltInFont.Helvetica)
+
+                For i As Integer = 0 To pdf.PageCount - 1
+                    Dim page As PdfPage = pdf.Pages(i)
+                    drawHeader(page, font)
+                    drawFooter(i, page, font)
+                Next
+
+                pdf.Save(PathToFile)
+            End Using
+
+            Process.Start(PathToFile)
+        End Sub
+
+        Private Shared Sub drawHeader(ByVal page As PdfPage, ByVal font As PdfFont)
+            Dim canvas = page.Canvas
+            canvas.Font = font
+            canvas.FontSize = 30
+
+            Const HeaderText As String = "Header"
+            Dim textWidth As Double = canvas.GetTextWidth(HeaderText)
+            Dim rotatedPageWidth As Double = getRotatedPageSize(page).Width
+            Dim headerPosition = New PdfPoint((rotatedPageWidth - textWidth) / 2, 10)
+
+            showTextAtRotatedPage(HeaderText, headerPosition, page)
+        End Sub
+
+        Private Shared Sub drawFooter(ByVal pageIndex As Integer, ByVal page As PdfPage, ByVal font As PdfFont)
+            Dim canvas = page.Canvas
+            canvas.Font = font
+            canvas.FontSize = 14
+
+            Dim rotatedPageSize As PdfSize = getRotatedPageSize(page)
+            Dim paddingFromCorner = New PdfSize(30, 30)
+            Dim positionRightBottom = New PdfPoint(rotatedPageSize.Width - paddingFromCorner.Width, rotatedPageSize.Height - paddingFromCorner.Height)
+
+            showTextAtRotatedPage((pageIndex + 1).ToString(), positionRightBottom, page)
+        End Sub
+
+        Private Shared Function getRotatedPageSize(ByVal page As PdfPage) As PdfSize
+            If page.Rotation = PdfRotation.Rotate90 OrElse page.Rotation = PdfRotation.Rotate270 Then
+                Return New PdfSize(page.Height, page.Width)
+            End If
+
+            Return New PdfSize(page.Width, page.Height)
+        End Function
+
+        Private Shared Sub showTextAtRotatedPage(ByVal text As String, ByVal position As PdfPoint, ByVal page As PdfPage)
+            Dim canvas As PdfCanvas = page.Canvas
+
+            If page.Rotation = PdfRotation.None Then
+                canvas.DrawString(position.X, position.Y, text)
+                Return
+            End If
+
+            canvas.SaveState()
+
+            Select Case page.Rotation
+                Case PdfRotation.Rotate90
+                    canvas.TranslateTransform(position.Y, page.Height - position.X)
+                Case PdfRotation.Rotate180
+                    canvas.TranslateTransform(page.Width - position.X, page.Height - position.Y)
+                Case PdfRotation.Rotate270
+                    canvas.TranslateTransform(page.Width - position.Y, position.X)
+            End Select
+
+            canvas.RotateTransform(CInt(page.Rotation) * 90)
+            canvas.DrawString(0, 0, text)
+
+            canvas.RestoreState()
+        End Sub
+    End Class
+End Namespace
