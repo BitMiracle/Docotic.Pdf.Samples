98d75755ac97d0873a95b0d7b3bc6527a40fa3f1
Vitaly Shibaev <shibvit@gmail.com>
2019-09-01 15:21:49 +0600

    Draft version of OcrAndMakeSearchable sample

---
+++ Samples/Text/OcrAndMakeSearchable/C#/OcrAndMakeSearchable.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+using Tesseract;
+
+namespace BitMiracle.Docotic.Pdf.Samples
+{
+    public static class OcrAndMakeSearchable
+    {
+        public static void Main()
+        {
+            // NOTE: 
+            // When used in trial mode, the library imposes some restrictions.
+            // Please visit http://bitmiracle.com/pdf-library/trial-restrictions.aspx
+            // for more information.
+
+            using (var pdf = new PdfDocument("Sample data/Freedman Scora.pdf"))
+            {
+                PdfFont universalFont = pdf.AddFont("Arial");
+                using (var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default))
+                {
+                    for (int i = 0; i < pdf.PageCount; ++i)
+                    {
+                        PdfPage page = pdf.Pages[i];
+                        string searchableText = page.GetText();
+
+                        // Simple check if the page contains searchable text.
+                        // We do not need to do OCR in that case.
+                        if (!string.IsNullOrEmpty(searchableText.Trim()))
+                            continue;
+
+                        // Save PDF page as high-resolution image
+                        PdfDrawOptions options = PdfDrawOptions.Create();
+                        options.BackgroundColor = new PdfRgbColor(255, 255, 255);
+
+                        const int Dpi = 600;
+                        options.HorizontalResolution = Dpi;
+                        options.VerticalResolution = Dpi;
+
+                        string pageImage = $"page_{i}.png";
+                        page.Save(pageImage, options);
+
+                        var canvas = page.Canvas;
+                        canvas.Font = universalFont;
+                        canvas.TextRenderingMode = PdfTextRenderingMode.NeitherFillNorStroke;
+
+                        const double ImageToPdfScaleFactor = 72.0 / Dpi;
+                        using (var img = Pix.LoadFromFile(pageImage))
+                        {
+                            using (var recognizedPage = engine.Process(img))
+                            {
+                                using (ResultIterator iter = recognizedPage.GetIterator())
+                                {
+                                    iter.Begin();
+                                    do
+                                    {
+                                        if (iter.TryGetBoundingBox(PageIteratorLevel.Word, out Rect wordBounds))
+                                        {
+                                            string wordText = iter.GetText(PageIteratorLevel.Word);
+
+                                            var position = new PdfPoint(
+                                                wordBounds.X1 * ImageToPdfScaleFactor,
+                                                wordBounds.Y1 * ImageToPdfScaleFactor
+                                            );
+                                            canvas.FontSize = wordBounds.Height * ImageToPdfScaleFactor;
+                                            canvas.DrawString(position, wordText);
+                                        }
+                                    } while (iter.Next(PageIteratorLevel.Word));
+                                }
+                            }
+                        }
+                    }
+                }
+
+                universalFont.RemoveUnusedGlyphs();
+
+                const string Result = "OcrAndMakeSearchable.pdf";
+                pdf.Save(Result);
+
+                Process.Start(Result);
+            }
+        }
+    }
+}
---
+++ Samples/Text/OcrAndMakeSearchable/README.md
@@ -0,0 +1,11 @@
+# OCR non-searchable PDF and convert to searchable document
+This sample shows how to create searchable PDF document from a non-searchable (image-based) document using Docotic.Pdf library and [Tesseract OCR Engine](https://github.com/charlesw/tesseract).
+
+Follow these steps to do OCR when PDF page does not contain searchable text:
+1. Save the page as high-resolution image using Docotic.Pdf. Higher resolution leads to better recognition quality.
+2. Recognize the image using Tesseract OCR engine. 
+3. Insert recognized text chunks back to PDF using Docotic.Pdf.
+
+If your documents contain text in language(s) other than English, then make sure to provide [Language Data Files for Tesseract 3.04/3.05](https://github.com/tesseract-ocr/tessdata/tree/3.04.00) for the language(s) of your document.
+
+Also ensure that you have [Visual Studio 2015 x86 & x64 runtimes](https://www.microsoft.com/en-us/download/details.aspx?id=48145) installed.
