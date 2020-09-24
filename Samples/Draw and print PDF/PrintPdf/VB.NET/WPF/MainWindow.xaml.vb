effc4fbf7d25a9c03bd119db6700a4e3f3c350fc
Vitaliy Shibaev <shibvit@gmail.com>
2020-09-24 14:53:02 +0600

    Add VB.NET version for PrintPdfWpf sample

---
+++ Samples/Draw and print PDF/PrintPdf/VB.NET/WPF/MainWindow.xaml.vb
@@ -0,0 +1,30 @@
+﻿Namespace BitMiracle.Docotic.Pdf.Samples
+    Class MainWindow
+        Private Const FitPageIndex As Integer = 0
+
+        Private Function getPrintSize() As PrintSize
+            Return If((PrintSizeCombobox.SelectedIndex = FitPageIndex), PrintSize.FitPage, PrintSize.ActualSize)
+        End Function
+
+        Private Sub Print_Click(sender As Object, e As RoutedEventArgs)
+            processExistingPdfDocument(AddressOf PdfPrintHelper.ShowPrintDialog)
+        End Sub
+
+        Private Sub Preview_Click(sender As Object, e As RoutedEventArgs)
+            processExistingPdfDocument(AddressOf PdfPrintHelper.ShowPrintPreview)
+        End Sub
+
+        Private Sub processExistingPdfDocument(ByVal action As Func(Of PdfDocument, PrintSize, Forms.DialogResult))
+            Using dlg = New Forms.OpenFileDialog()
+                dlg.Filter = "PDF files (*.pdf)|*.pdf"
+
+                If dlg.ShowDialog() = Forms.DialogResult.OK Then
+
+                    Using pdf = New PdfDocument(dlg.FileName)
+                        action(pdf, getPrintSize())
+                    End Using
+                End If
+            End Using
+        End Sub
+    End Class
+End Namespace
---
+++ Samples/Draw and print PDF/PrintPdf/VB.NET/WPF/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Globalization
+Imports System.Resources
+Imports System.Windows
+
+<Assembly: AssemblyTitle("PrintPdfWpf")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Bit Miracle")>
+<Assembly: AssemblyProduct("PrintPdfWpf")>
+<Assembly: AssemblyCopyright("Copyright © 2014 - 2020 Bit Miracle")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: ComVisible(false)>
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+<Assembly: Guid("410258c7-ba6e-4f4a-907e-e49bff9829e8")>
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
