9a56a2b6c1cf618f45035fcd2b39bd26b17169da
Vitaly Shibaev <shibvit@gmail.com>
2021-09-02 17:45:34 +0600

    Add RemovePaths sample

---
+++ Samples/Graphics/RemovePaths/C#/RemovePaths.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+
+using BitMiracle.Docotic.Pdf;
+
+namespace BitMiracle.Docotic.Pdf.Samples
+{
+    public static class RemovePaths
+    {
+        public static void Main()
+        {
+            // NOTE: 
+            // When used in trial mode, the library imposes some restrictions.
+            // Please visit http://bitmiracle.com/pdf-library/trial-restrictions.aspx
+            // for more information.
+            
+            const string PathToFile = "RemovePaths.pdf";
+
+            using (var pdf = new PdfDocument(@"Sample Data\form.pdf"))
+            {
+                PdfPage page = pdf.Pages[0];
+                page.RemovePaths(
+                    path =>
+                    {
+                        // remove the form header filled with orange (255, 127, 64)
+                        if (path.PaintMode == PdfDrawMode.Stroke)
+                            return false;
+
+                        return (
+                            path.Brush.Color is PdfRgbColor fillColor &&
+                            fillColor.R == 255 &&
+                            fillColor.G == 127 &&
+                            fillColor.B == 64
+                        );
+                    }
+                );
+
+                pdf.Save(PathToFile);
+            }
+
+            Process.Start(PathToFile);
+        }
+    }
+}
---
+++ Samples/Graphics/RemovePaths/README.md
@@ -0,0 +1,9 @@
+# Remove paths from PDF documents in C# and VB.NET
+This sample shows how to remove vector paths from a PDF page using [Docotic.Pdf library](https://bitmiracle.com/pdf-library/).
+
+Use [PdfPage.RemovePaths()](https://bitmiracle.com/pdf-library/help/pdfpage.removepaths.html) method to remove all or specific vector paths from a PDF page. You can filter paths by color, shape, drawing mode, or other parameters.
+
+## See also
+* [Get free time-limited license key](https://bitmiracle.com/pdf-library/download-pdf-library.aspx)
+* [Remove painted images](/Samples/Images/RemovePaintedImages) sample
+* [Copy page objects](/Samples/Pages%20and%20Navigation/CopyPageObjects) sample
