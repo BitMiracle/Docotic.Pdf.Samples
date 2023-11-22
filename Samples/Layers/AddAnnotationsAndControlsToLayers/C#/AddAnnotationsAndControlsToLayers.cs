86476ef3e5f256dc22ad8fb3bd3e951652b99d5f
Vitaly Shibaev <shibvit@gmail.com>
2023-11-22 11:43:16 +0600

    Enable C# nullable reference types and cleanup code in "Layers" samples

--- Samples/Layers/AddAnnotationsAndControlsToLayers/C#/AddAnnotationsAndControlsToLayers.cs
+++ Samples/Layers/AddAnnotationsAndControlsToLayers/C#/AddAnnotationsAndControlsToLayers.cs
@@ -14,7 +14,7 @@ namespace BitMiracle.Docotic.Pdf.Samples
             
             string pathToFile = "AddAnnotationsAndControlsToLayers.pdf";
 
-            using (PdfDocument pdf = new PdfDocument())
+            using (var pdf = new PdfDocument())
             {
                 pdf.PageMode = PdfPageMode.UseOC;
 
